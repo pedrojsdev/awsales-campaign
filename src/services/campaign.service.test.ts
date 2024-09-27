b0966377b65dc74711bdb90ce3d0b475ef3c32da
Pedro <pedrojs.contato@gmail.com>
2024-09-27 15:03:36 -0300

    test: finish unit tests

--- src/services/campaign.service.test.ts
+++ src/services/campaign.service.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { CampaignService } from './campaign'
 import { InMemoryCamapignRepository } from '../repositories/inMemory/inMemoryCampaignRepository'
 import { CreateCampaignDTO } from '../dtos/createCampaign'
@@ -12,6 +12,7 @@ describe("CampaignService", () => {
     beforeEach(() => {
         campaignRepository = new InMemoryCamapignRepository()
         campaignService = new CampaignService(campaignRepository)
+        vi.resetAllMocks()
     })
 
     it("should create a campaign with valid data", async () => {
@@ -84,6 +85,76 @@ describe("CampaignService", () => {
 
     it("should throw an error when deleting a non-existing campaign", async () => {
         await expect(campaignService.deleteCampaign(0)).rejects.toThrow("Campanha não encontrada.")
+    })
+
+    it("should throw an error if dataInicio is in the past at updating a campaign", async () => {
+        const validCampaign: CreateCampaignDTO = {
+            categoria: "Marketing",
+            dataInicio: new Date(Date.now() + 3600 * 1000),
+            dataFim: new Date(Date.now() + 7200 * 1000),
+            nome: "Test",
+        }
+
+        const campaign = await campaignService.createCampaign(validCampaign)
+
+        await expect(campaignService.updateCampaign(campaign.id, {
+            dataInicio: new Date(Date.now() - 3600 * 1000),
+        })).rejects.toThrow("A data de início deve ser igual ou posterior à data atual.")
+    })
+
+    it("should throw an error if dataFim is before dataInicio on update", async () => {
+        const validCampaign: CreateCampaignDTO = {
+            categoria: "Marketing",
+            dataInicio: new Date(Date.now() + 3600 * 1000),
+            dataFim: new Date(Date.now() + 7200 * 1000),
+            nome: "Test",
+        }
+
+        const campaign = await campaignService.createCampaign(validCampaign)
+
+        await expect(campaignService.updateCampaign(campaign.id, {
+            dataFim: new Date(Date.now() + 3600 * 1000),
+            dataInicio: new Date(Date.now() + 14400 * 1000)
+        })).rejects.toThrow("A data de fim deve ser maior que a data de início.")
+    })
+
+    it('should mark the campaign as "expirada" when the updated dataFim is in the past', async () => {
+
+        // Mock da data atual (now)
+        const mockNow = new Date('2023-12-01');
+        vi.setSystemTime(mockNow);
+
+        const validCampaign: CreateCampaignDTO = {
+            categoria: "Marketing",
+            dataInicio: new Date(mockNow.getTime() + 3600 * 1000),
+            dataFim: new Date(mockNow.getTime() + 7200 * 1000),
+            nome: "Test",
+        }
+
+        const campaign = await campaignService.createCampaign(validCampaign)
+        
+        vi.useRealTimers()
+        const updatedCampaign = await campaignService.updateCampaign(campaign.id, {
+            dataFim: new Date(Date.now() - 3600 * 1000)
+        })
+        
+        expect(updatedCampaign.status).toBe("expirada")
+    })
+
+    it("should throw an error if dataFim is before dataInicio on update", async () => {
+        const validCampaign: CreateCampaignDTO = {
+            categoria: "Marketing",
+            dataInicio: new Date(Date.now() + 3600 * 1000),
+            dataFim: new Date(Date.now() + 7200 * 1000),
+            nome: "Test",
+        }
+
+        const campaign = await campaignService.createCampaign(validCampaign)
+
+        await expect(campaignService.updateCampaign(campaign.id, {
+            dataFim: new Date(Date.now() + 3600 * 1000),
+            dataInicio: new Date(Date.now() + 14400 * 1000)
+        })).rejects.toThrow("A data de fim deve ser maior que a data de início.")
     })
 
 })
